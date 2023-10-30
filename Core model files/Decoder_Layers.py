ec4cee94c3f6c17454f0f947ef2317191f20dcd3
MachtaYassine <you@example.commachtayassine@gmail.com>
2023-10-30 21:52:26 +0100

    comments

--- Core model files/Decoder_Layers.py
+++ Core model files/Decoder_Layers.py
@@ -41,7 +41,15 @@ class DecoderLayer(nn.Module):  # for text only, we use the classical attention 
     def forward(self, x, e_output, tgt_mask, tgt_key_padding_mask, e_key_padding_mask):
         """
         inputs:
-         x:
+            x: the target, "the output that we want to predict" offset by one position?
+            e_output: the output of the encoder
+            tgt_mask: the mask for the target to avoid the model to look at the future (next words)
+            tgt_key_padding_mask: the mask for the target so that attention does not look at the padding. We used padding to make sure all inputs have the same sequence lengths during batching.
+            e_key_padding_mask: the mask for the encoder so that attention does not look at the padding. Same reason.
+        outputs:
+            x: The predicted ???
+            attn_weights: the attention weights of the second attention layer
+
         """
         x_1 = self.norm_1(x)
         x = x + self.dropout_1(
@@ -60,9 +68,18 @@ class DecoderLayer(nn.Module):  # for text only, we use the classical attention 
         return x, attn_weights
 
 
-class Multimodal_DecoderLayer(
-    nn.Module
-):  # we use the multimodal attention layer. See file Multimodal_attention.py
+class Multimodal_DecoderLayer(nn.Module):
+    """
+    Decoder layer of the transformer model. It is composed of 3 normalisation layers, 3 dropout layers, 2 attention layers and 1 feedforward layer.
+    inputs:
+        d_model: dimension of the model
+        heads: number of heads in the multihead attention layers
+        dim_feedforward: dimension of the feedforward layer
+        dropout: dropout rate
+        is_there_image: boolean to know if there is an image or not
+
+    """
+
     def __init__(
         self, d_model, heads, dim_feedforward, dropout=0.1, is_there_image=True
     ):
